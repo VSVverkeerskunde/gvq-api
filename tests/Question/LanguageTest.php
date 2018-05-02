ad132743d1c997f1e7978a9309cabafe2bb54ab7
Steven <obex_nielssen@hotmail.com>
2018-05-02 11:01:47 +0200

    VSVGVQ-7: Add test for invalid language argument

--- tests/Question/LanguageTest.php
+++ tests/Question/LanguageTest.php
@@ -12,8 +12,24 @@ class LanguageTest extends TestCase
     public function it_only_accepts_supported_languages()
     {
         $language = new Language('nl');
+
         $this->assertNotNull($language);
+
         $language = new Language('fr');
+        
         $this->assertNotNull($language);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_for_unsupported_languages()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Given language ge is not supported, only nl en fr are allowed.'
+        );
+
+        new Language('ge');
     }
 }
