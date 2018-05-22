d976d86a550059be6e2a64d8a0ca33df9276da4a
Steven <obex_nielssen@hotmail.com>
2018-05-22 15:16:11 +0200

    VSVGVQ-20: add password value object to user

---
+++ src/User/ValueObjects/Password.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace VSV\GVQ_API\User\ValueObjects;
+
+class Password
+{
+    /**
+     * @var string
+     */
+    private $value;
+
+    private function __construct()
+    {
+    }
+
+    public static function fromHash(string $hash)
+    {
+        $password = new Password();
+        $password->setValue($hash);
+
+        return $password;
+    }
+
+    public static function fromPlainText(string $plainTextValue)
+    {
+        if (!preg_match('/^(?=[^ ])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z])(.{8,})(?<=\S)$/', $plainTextValue)) {
+            throw new \InvalidArgumentException(
+                'Invalid value for password. '.
+                'Must be at least 8 characters long, contain at least one lowercase, '.
+                'one uppercase and one non-alphabetical character and must not start or end with a space.'
+            );
+        }
+
+        $password = new Password();
+        $password->setValue(password_hash($plainTextValue, PASSWORD_DEFAULT));
+
+        return $password;
+    }
+
+    private function setValue(string $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function toNative(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $inputPassword
+     * @return bool
+     */
+    public function verifies(string $inputPassword): bool
+    {
+        return password_verify($inputPassword, $this->value);
+    }
+}
