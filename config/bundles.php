c9cbdc2e5d9f3d58c8175b6c06b62d228ba59af5
Luc Wollants <luc.wollants@apptite.be>
2018-05-27 15:47:10 +0200

    VSVGVQ-27 Initial setup of monolog for Symfony.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     VSV\GVQ_API\GVQBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
