cbc418779c7de737f006c20242ee0a5a3e83875f
Steven <obex_nielssen@hotmail.com>
2018-06-07 15:06:01 +0200

    VSVGVQ-54: add all fields with input validation to registration form

--- src/Account/Controllers/AccountViewController.php
+++ src/Account/Controllers/AccountViewController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Translation\TranslatorInterface;
 use VSV\GVQ_API\Account\Forms\RegistrationFormType;
 
 class AccountViewController extends AbstractController
@@ -16,13 +17,23 @@ class AccountViewController extends AbstractController
     private $registrationFormType;
 
     /**
+     * @var TranslatorInterface
      */
-    public function __construct()
+    private $tranlator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
     {
+        $this->tranlator = $translator;
         $this->registrationFormType = new RegistrationFormType();
     }
 
-
+    /**
+     * @param Request $request
+     * @return Response
+     */
     public function register(Request $request): Response
     {
         $form = $this->createRegisterForm();
@@ -36,13 +47,18 @@ class AccountViewController extends AbstractController
         );
     }
 
+    /**
+     * @return FormInterface
+     */
     private function createRegisterForm(): FormInterface
     {
         $formBuilder = $this->createFormBuilder();
 
         $this->registrationFormType->buildForm(
           $formBuilder,
-          []
+          [
+              'translator' => $this->tranlator,
+          ]
         );
 
         return $formBuilder->getForm();
--- src/Account/Forms/RegistrationFormType.php
+++ src/Account/Forms/RegistrationFormType.php
@@ -3,20 +3,190 @@
 namespace VSV\GVQ_API\Account\Forms;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        /** @var TranslatorInterface $translator */
+        $translator = $options['translator'];
+
         $builder
             ->add(
                 'email',
                 TextType::class,
                 [
-                    'label' => false
+                    'constraints' => [
+                        new Email(
+                            [
+                                'message' => $translator->trans('Geef een geldig e-mailadres.'),
+                            ]
+                        ),
+                        new NotBlank(
+                            [
+                                'message' => $translator->trans('Het e-mailveld mag niet leeg zijn.'),
+                            ]
+                        ),
+                    ],
+                ]
+            )
+            ->add(
+                'password',
+                RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'invalid_message' => $translator->trans('De paswoorden komen niet overeen.'),
+                    'help' => 'test help',
+                    'first_options' => [
+
+                        'constraints' => [
+                            new Regex(
+                                [
+                                    'pattern' => '/^(?=[^ ])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z])(.{8,})(?<=\S)$/',
+                                    'message' => $translator->trans(
+                                        'Het wachtwoord moet minstens 8 karakters lang zijn en minstens één kleine letter, hoofdletter en ander karakter bevatten'
+                                    ),
+                                ]
+                            ),
+                            new NotBlank(
+                                [
+                                    'message' => $translator->trans('Het wachtwoord mag niet leeg zijn.'),
+                                ]
+                            ),
+                            new Length(
+                                [
+                                    'max' => 4096,
+                                    'maxMessage' => $translator->trans(
+                                        'Het wachtwoord mag maximum {{ limit }} karakters bevatten'
+                                    ),
+                                ]
+                            ),
+                        ],
+                    ],
+                    'second_options' => [],
+                ]
+            )
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'constraints' => $this->createNameConstraints($translator),
+                ]
+            )
+            ->add(
+                'firstName',
+                TextType::class,
+                [
+                    'constraints' => $this->createNameConstraints($translator),
+                ]
+            )
+            ->add(
+                'companyName',
+                TextType::class,
+                [
+                    'constraints' => $this->createNameConstraints($translator),
+                ]
+            )
+            ->add(
+                'numberOfEmployees',
+                IntegerType::class,
+                [
+                    'constraints' => [
+                        new GreaterThan(
+                            [
+                                'value' => 0,
+                                'message' => $translator->trans('Het aantal werknemers moet groter zijn dan 0.'),
+                            ]
+                        ),
+                        new NotBlank(
+                            [
+                                'message' => $translator->trans('Het aantal werknemers mag niet leeg zijn.'),
+                            ]
+                        ),
+                    ],
+                ]
+            )
+            ->add(
+                'aliasNl',
+                TextType::class,
+                [
+                    'constraints' => $this->createAliasConstraints($translator),
+                ]
+            )
+            ->add(
+                'aliasFr',
+                TextType::class,
+                [
+                    'constraints' => $this->createAliasConstraints($translator),
                 ]
             );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'translator' => null,
+            ]
+        );
+    }
+
+    /**
+     * @param TranslatorInterface $translator
+     * @return array
+     */
+    private function createNameConstraints(TranslatorInterface $translator): array
+    {
+        return [
+            new NotBlank(
+                [
+                    'message' => $translator->trans('Dit veld mag niet leeg zijn.'),
+                ]
+            ),
+            new Length(
+                [
+                    'max' => 255,
+                    'maxMessage' => $translator->trans('Dit veld mag maximum {{ limit }} karakters bevatten.'),
+                ]
+            ),
+        ];
+    }
+
+    /**
+     * @param TranslatorInterface $translator
+     * @return array
+     */
+    private function createAliasConstraints(TranslatorInterface $translator): array
+    {
+        return [
+            new NotBlank(
+                [
+                    'message' => $translator->trans('Dit veld mag niet leeg zijn.'),
+                ]
+            ),
+            new Length(
+                [
+                    'min' => 3,
+                    'max' => 255,
+                    'maxMessage' => $translator->trans('Dit veld mag maximum {{ limit }} karakters bevatten.'),
+                    'minMessage' => $translator->trans('Dit veld moet minimum {{ limit }} karakters bevatten.'),
+                ]
+            ),
+        ];
     }
 }
