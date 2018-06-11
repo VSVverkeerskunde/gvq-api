c3f73c181d4b80541894b4c96262e17fdc13b988
Luc Wollants <luc.wollants@apptite.be>
2018-06-11 13:38:49 +0200

    VSVGVQ-59 Add a basic export to CSV for users.

--- src/User/Controllers/UserViewController.php
+++ src/User/Controllers/UserViewController.php
@@ -4,6 +4,7 @@ namespace VSV\GVQ_API\User\Controllers;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 use VSV\GVQ_API\User\Repositories\UserRepository;
 
 class UserViewController extends AbstractController
@@ -14,11 +15,20 @@ class UserViewController extends AbstractController
     private $userRepository;
 
     /**
-     * @param UserRepository $userRepository
+     * @var SerializerInterface
      */
-    public function __construct(UserRepository $userRepository)
-    {
+    private $serializer;
+
+    /**
+     * @param UserRepository $userRepository
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(
+        UserRepository $userRepository,
+        SerializerInterface $serializer
+    ) {
         $this->userRepository = $userRepository;
+        $this->serializer = $serializer;
     }
 
     /**
@@ -34,5 +44,55 @@ class UserViewController extends AbstractController
                 'users' => $users ? $users->toArray(): [],
             ]
         );
+    }
+
+    /**
+     * @return Response
+     */
+    public function export(): Response
+    {
+        $users = $this->userRepository->getAll();
+        $usersAsCsv = $this->serializer->serialize($users, 'csv');
+
+        $usersAsCsv = $this->createCsvData($usersAsCsv);
+        $response = $this->createCsvResponse($usersAsCsv);
+
+        return $response;
+    }
+
+    /**
+     * @param string $data
+     * @return string
+     */
+    private function createCsvData(string $data): string
+    {
+        /**
+         * @see: https://github.com/thephpleague/csv/blob/507815707cbdbebaf076873bff04cd6ad65fe0fe/docs/9.0/connections/bom.md
+         */
+        $csvData = chr(0xFF).chr(0xFE);
+        $csvData .= mb_convert_encoding('sep=,'.PHP_EOL.$data, 'UTF-16LE', 'UTF-8');
+        return $csvData;
+    }
+
+    /**
+     * @param string $csvData
+     * @return Response
+     */
+    private function createCsvResponse(string $csvData): Response
+    {
+        $response = new Response($csvData);
+
+        $response->headers->set('Content-Encoding', 'UTF-8');
+        $response->headers->set('Content-Type', 'application/csv; charset=UTF-8');
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+
+        $now = new \DateTime();
+        $fileName = 'users_'.$now->format(\DateTime::ATOM).'.csv';
+        $response->headers->set(
+            'Content-Disposition',
+            'attachment; filename="'.$fileName.'"'
+        );
+
+        return $response;
     }
 }
