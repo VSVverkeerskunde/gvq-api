0432ea056f39685e80ceace7a0459b4d212eec04
Luc Wollants <luc.wollants@apptite.be>
2018-06-11 09:17:23 +0200

    VSVGVQ-59 Basic list of all contacts (with bootstrap-table).

--- src/User/Controllers/UserViewController.php
+++ src/User/Controllers/UserViewController.php
@@ -4,16 +4,35 @@ namespace VSV\GVQ_API\User\Controllers;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use VSV\GVQ_API\User\Repositories\UserRepository;
 
 class UserViewController extends AbstractController
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * @return Response
      */
     public function index(): Response
     {
+        $users = $this->userRepository->getAll();
+
         return $this->render(
-            'users/index.html.twig'
+            'users/index.html.twig',
+            [
+                'users' => $users ? $users->toArray(): [],
+            ]
         );
     }
 }
