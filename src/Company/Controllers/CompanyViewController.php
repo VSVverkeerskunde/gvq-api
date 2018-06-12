763c982083ae74fa32e882cbccd020e3d5ba495d
Luc Wollants <luc.wollants@apptite.be>
2018-06-12 14:35:54 +0200

    VSVGVQ-53 Add basic table for companies to company index view.

--- src/Company/Controllers/CompanyViewController.php
+++ src/Company/Controllers/CompanyViewController.php
@@ -4,16 +4,35 @@ namespace VSV\GVQ_API\Company\Controllers;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use VSV\GVQ_API\Company\Repositories\CompanyRepository;
 
 class CompanyViewController extends AbstractController
 {
+    /**
+     * @var CompanyRepository
+     */
+    private $companyRepository;
+
+    /**
+     * @param CompanyRepository $companyRepository
+     */
+    public function __construct(CompanyRepository $companyRepository)
+    {
+        $this->companyRepository = $companyRepository;
+    }
+
     /**
      * @return Response
      */
     public function index(): Response
     {
+        $companies = $this->companyRepository->getAll();
+
         return $this->render(
-            'companies/index.html.twig'
+            'companies/index.html.twig',
+            [
+                'companies' => $companies ? $companies->toArray(): [],
+            ]
         );
     }
 }
