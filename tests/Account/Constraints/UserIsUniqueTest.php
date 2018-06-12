45b43a96d4d1815f40aeaba6e611e7361b1e7ab1
Steven <obex_nielssen@hotmail.com>
2018-06-12 10:09:49 +0200

    VSVGVQ-54: add test for UserIsUnique constraint

---
+++ tests/Account/Constraints/UserIsUniqueTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace VSV\GVQ_API\Account\Constraints;
+
+use PHPUnit\Framework\TestCase;
+
+class UserIsUniqueTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_the_correct_validator_class(): void
+    {
+        $uniqeUserConstraint = new UserIsUnique();
+
+        $this->assertEquals(
+          'VSV\GVQ_API\Account\Constraints\UserIsUniqueValidator',
+          $uniqeUserConstraint->validatedBy()
+        );
+    }
+}
