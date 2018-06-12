3d37a9caf982b9a503a54017fb2c0406b292700d
Steven <obex_nielssen@hotmail.com>
2018-06-12 17:47:53 +0200

    VSVGVQ-54: add tests for CompanyRepository getByName and getByAlias

--- tests/Company/Repositories/CompanyDoctrineRepositoryTest.php
+++ tests/Company/Repositories/CompanyDoctrineRepositoryTest.php
@@ -4,8 +4,10 @@ namespace VSV\GVQ_API\Company\Repositories;
 
 use Ramsey\Uuid\Uuid;
 use VSV\GVQ_API\Common\Repositories\AbstractDoctrineRepositoryTest;
+use VSV\GVQ_API\Common\ValueObjects\NotEmptyString;
 use VSV\GVQ_API\Company\Models\Company;
 use VSV\GVQ_API\Company\Repositories\Entities\CompanyEntity;
+use VSV\GVQ_API\Company\ValueObjects\Alias;
 use VSV\GVQ_API\Factory\ModelsFactory;
 use VSV\GVQ_API\User\Repositories\UserDoctrineRepository;
 
@@ -75,5 +77,75 @@ class CompanyDoctrineRepositoryTest extends AbstractDoctrineRepositoryTest
         );
 
         $this->companyDoctrineRepository->save($company);
+    }
+
+    /**
+     * @test
+     * @param NotEmptyString $name
+     * @param Company|null $expectedResult
+     * @dataProvider nameProvider
+     */
+    public function it_can_get_a_company_by_name(NotEmptyString $name, ?Company $expectedResult): void
+    {
+        $this->companyDoctrineRepository->save($this->company);
+
+        $foundCompany = $this->companyDoctrineRepository->getByName($name);
+
+        $this->assertEquals(
+            $expectedResult,
+            $foundCompany
+        );
+    }
+
+    /**
+     * @return array[]
+     */
+    public function nameProvider(): array
+    {
+        return [
+            [
+                new NotEmptyString('Vlaamse Stichting Verkeerskunde'),
+                ModelsFactory::createCompany(),
+            ],
+            [
+                new NotEmptyString('alternate'),
+                null,
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @param Alias $alias
+     * @param Company|null $expectedResult
+     * @dataProvider aliasProvider
+     */
+    public function it_can_get_a_company_by_alias(Alias $alias, ?Company $expectedResult): void
+    {
+        $this->companyDoctrineRepository->save($this->company);
+
+        $foundCompany = $this->companyDoctrineRepository->getByAlias($alias);
+
+        $this->assertEquals(
+            $expectedResult,
+            $foundCompany
+        );
+    }
+
+    /**
+     * @return array[]
+     */
+    public function aliasProvider(): array
+    {
+        return [
+            [
+                new Alias('vsv'),
+                ModelsFactory::createCompany(),
+            ],
+            [
+                new Alias('alternate'),
+                null,
+            ],
+        ];
     }
 }
