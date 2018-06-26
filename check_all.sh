8768d7b08f18e08c4143c579b8ef76ef38fe7b8e
Luc Wollants <luc.wollants@apptite.be>
2018-06-26 09:56:39 +0200

    VSVGVQ-63 Disable code coverage for check_all command.

--- check_all.sh
+++ check_all.sh
@@ -1,5 +1,5 @@
 ./check_strict_types.sh
 ./vendor/bin/phpstan --level=7 analyse src tests
 ./vendor/bin/phpcs
-./vendor/bin/phpunit
+./vendor/bin/phpunit --no-coverage
 ./bin/console lint:twig templates/
