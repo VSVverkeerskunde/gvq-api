e23c2d7727b5ea6a2cda1589b5b750cfc038f430
Steven <obex_nielssen@hotmail.com>
2018-06-27 17:01:11 +0200

    VSVGVQ-60 add functionality to send activation mail from user list

--- src/Account/Controllers/AccountViewController.php
+++ src/Account/Controllers/AccountViewController.php
@@ -3,6 +3,7 @@
 namespace VSV\GVQ_API\Account\Controllers;
 
 use Psr\Log\LoggerInterface;
+use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidFactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
@@ -333,6 +334,61 @@ class AccountViewController extends AbstractController
         } else {
             return $this->render('accounts/activation_error.html.twig');
         }
+    }
+
+
+    /**
+     * @param Request $request
+     * @param string $id
+     * @return Response
+     * @throws \Exception
+     */
+    public function sendActivation(Request $request, string $id): Response
+    {
+        $user = $this->userRepository->getById(Uuid::fromString($id));
+        if (!$user) {
+            $this->addFlash('success', $this->translator->trans('User.id.invalid', ['%id%' => $id]));
+
+            return $this->redirectToRoute('users_view_index');
+        }
+
+        if ($request->getMethod() === 'POST') {
+            $existingRegistration = $this->registrationRepository->getByUserId($user->getId());
+            if ($existingRegistration) {
+                $this->registrationRepository->delete(
+                    $existingRegistration->getId()
+                );
+            }
+
+            $registration = $this->registrationFormType->createRegistrationForUser(
+                $this->uuidFactory,
+                $this->urlSuffixGenerator,
+                $user,
+                false
+            );
+            $this->registrationRepository->save($registration);
+
+            $this->mailService->sendActivationMail($registration);
+
+            $this->addFlash(
+                'success',
+                $this->translator->trans(
+                    'Activation.send.success',
+                    [
+                        '%email%' => $user->getEmail()->toNative(),
+                    ]
+                )
+            );
+
+            return $this->redirectToRoute('users_view_index');
+        }
+
+        return $this->render(
+            'users/send_activation.html.twig',
+            [
+                'email' => $user->getEmail()->toNative(),
+            ]
+        );
     }
 
     /**
