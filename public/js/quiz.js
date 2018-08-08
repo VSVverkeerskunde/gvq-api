e93037d3d3c8fa14213ef450b6537e6e0b278a3f
Bram Cordie <bramcordie@gmail.com>
2018-08-08 12:01:53 +0200

    VSVGVQ-118: Show question when image is loaded

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -11,15 +11,18 @@
   let Quiz =  function (quizConfig) {
     quizConfig = quizConfig || defaultQuizConfig;
     let view = $('#gvq-quiz .gvq-quiz-view');
+    let oldView = $('#gvq-quiz .gvq-quiz-old-view');
 
     function renderView(viewName, ...args) {
       let oldContent = view.children();
       let newContent = $(views[viewName].template).hide();
+
       function showNewContent() {
         oldContent.remove();
         newContent.show();
       }
 
+      oldView.append(oldContent);
       view.append(newContent);
       views[viewName].controller(...args).done(showNewContent);
     }
@@ -49,10 +52,11 @@
       askQuestion: {
         controller: function (quizId, questionNr) {
           let deferredRender = $.Deferred();
+          let counterInterval;
 
           function startCountdown() {
             let counter = view.find('.gvq-time-left');
-            let counterInterval = window.setInterval(function () {
+            counterInterval = window.setInterval(function () {
               let secondsLeft = parseInt(counter.text(),10) - 1;
               if (0 === secondsLeft) {
                 clearInterval(counterInterval);
@@ -63,6 +67,8 @@
           }
 
           function renderQuestion(data) {
+            let imageLocation = quizConfig.imageDirectory + data.imageFileName;
+            let questionImage = new Image();
             setViewValue('questionText', data.text);
             $.each(data.answers, function (index, answer) {
               setViewValue('answer'+answer.index, answer.text);
@@ -72,15 +78,18 @@
               .find('ul.gvq-answers')
               .on('click', 'li', function () {
                 let chosenAnswer = data.answers[$(this).index()];
+                clearInterval(counterInterval);
                 renderView('showAnswer', quizId, questionNr, chosenAnswer.id);
               });
 
-            view
-              .find('[data-src="question-image"]')
-              .attr('src', quizConfig.imageDirectory + data.imageFileName);
-
-            deferredRender.resolve();
-            startCountdown();
+            questionImage.onload = function () {
+              view
+                .find('.gvq-question-image')
+                .attr('src', imageLocation);
+              deferredRender.resolve();
+              startCountdown();
+            };
+            questionImage.src = imageLocation;
           }
 
           setViewValue('questionNr', questionNr);
@@ -103,7 +112,7 @@
             });
 
             view
-              .find('[data-src="question-image"]')
+              .find('.gvq-question-image')
               .attr('src', quizConfig.imageDirectory + data.imageFileName);
 
             setViewValue('questionText', data.text);
