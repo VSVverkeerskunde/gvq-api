bb0e1ac73c9e58e80e4630f40f19789e4276b482
Bram Cordie <bramcordie@gmail.com>
2018-08-08 10:07:57 +0200

    VSVGVQ-118: Deferred rendering and countdown

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -12,8 +12,15 @@
     let view = $('#gvq-quiz .gvq-quiz-view');
 
     function renderView(viewName, ...args) {
-      view.html(views[viewName].template);
-      views[viewName].controller(...args);
+      let oldContent = view.children();
+      let newContent = $(views[viewName].template).hide();
+      function showNewContent() {
+        oldContent.remove();
+        newContent.show();
+      }
+
+      view.append(newContent);
+      views[viewName].controller(...args).done(showNewContent);
     }
 
     function setViewValue(name, value) {
@@ -32,12 +39,28 @@
 
           $('button.gvq-start-button').on('click', function () {
             start($('input.gvq-participant-email').val());
-          })
+          });
+
+          return $.Deferred().resolve().promise();
         },
         template: $('div[data-template="participation-form"]').prop('outerHTML')
       },
       askQuestion: {
         controller: function (quizId, questionNr) {
+          let deferredRender = $.Deferred();
+
+          function startCountdown() {
+            let counter = view.find('.gvq-time-left');
+            let counterInterval = window.setInterval(function () {
+              let secondsLeft = parseInt(counter.text(),10) - 1;
+              if (0 === secondsLeft) {
+                clearInterval(counterInterval);
+                view.find('.gvq-countdown').addClass('finished');
+              }
+              counter.text(secondsLeft);
+            }, 1000);
+          }
+
           function renderQuestion(data) {
             setViewValue('questionText', data.text);
             $.each(data.answers, function (index, answer) {
@@ -49,16 +72,22 @@
               .on('click', 'li', function () {
                 let chosenAnswer = data.answers[$(this).index()];
                 renderView('showAnswer', quizId, questionNr, chosenAnswer.id);
-              })
+              });
+
+            deferredRender.resolve();
+            startCountdown();
           }
 
           setViewValue('questionNr', questionNr);
           $.get('/quiz/'+quizId+'/question').done(renderQuestion);
+          return deferredRender.promise();
         },
         template: $('div[data-template="ask-question"]').prop('outerHTML')
       },
       showAnswer: {
         controller: function(quizId, questionNr, answerId) {
+          let deferredRender = $.Deferred();
+
           function renderAnsweredQuestion(data) {
             setViewValue('questionText', data.text);
             $.each(data.answers, function (index, answer) {
@@ -70,13 +99,17 @@
 
             setViewValue('questionText', data.text);
             setViewValue('feedback', data.feedback);
+
+            deferredRender.resolve();
           }
 
           setViewValue('questionNr', questionNr);
           $.post('/quiz/'+quizId+'/question/'+answerId).done(renderAnsweredQuestion);
           $('button.gvq-next-question').on('click', function () {
             renderView('askQuestion', quizId, ++questionNr);
-          })
+          });
+
+          return deferredRender.promise();
         },
         template: $('div[data-template="show-answer"]').prop('outerHTML')
       }
