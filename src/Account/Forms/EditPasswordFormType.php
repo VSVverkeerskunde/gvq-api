9ebc527ccc88b4001e40b47d01ab97f68a00229b
Steven <obex_nielssen@hotmail.com>
2018-08-22 16:29:01 +0200

    VSVGVQ-132 refactor password formtypes

--- src/Account/Forms/EditPasswordFormType.php
+++ src/Account/Forms/EditPasswordFormType.php
@@ -2,25 +2,17 @@
 
 namespace VSV\GVQ_API\Account\Forms;
 
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
-use Symfony\Component\Translation\TranslatorInterface;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Regex;
 use VSV\GVQ_API\User\Models\User;
 use VSV\GVQ_API\User\ValueObjects\Password;
 
-class EditPasswordFormType extends AbstractType
+class EditPasswordFormType extends PasswordFormType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        /** @var TranslatorInterface $translator */
-        $translator = $options['translator'];
+        parent::buildForm($builder, $options);
 
         $builder
             ->add(
@@ -30,59 +22,15 @@ class EditPasswordFormType extends AbstractType
                     'constraints' => [
                         new UserPassword(
                             [
-                                'message' => $translator->trans('Field.password.old.incorrect'),
+                                'message' => $this->translator->trans('Field.password.old.incorrect'),
                             ]
                         ),
                     ],
                     'always_empty' => false,
                 ]
-            )
-            ->add(
-                'password',
-                RepeatedType::class,
-                [
-                    'type' => PasswordType::class,
-                    'invalid_message' => $translator->trans('Field.passwords.different'),
-                    'first_options' => [
-                        'always_empty' => false,
-                        'constraints' => [
-                            new Regex(
-                                [
-                                    'pattern' => Password::PATTERN,
-                                    'message' => $translator->trans('Field.password.pattern'),
-                                ]
-                            ),
-                            new NotBlank(
-                                [
-                                    'message' => $translator->trans('Field.empty'),
-                                ]
-                            ),
-                            new Length(
-                                [
-                                    'max' => 4096,
-                                    'maxMessage' => $translator->trans('Field.length.max'),
-                                ]
-                            ),
-                        ],
-                    ],
-                    'second_options' => [
-                        'always_empty' => false,
-                    ],
-                ]
             );
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(
-            [
-                'translator' => null,
-            ]
-        );
-    }
 
     /**
      * @param User $user
