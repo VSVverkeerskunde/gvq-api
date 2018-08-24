7465b9a4a574bcea3d734628b50ceb99e832d422
Luc Wollants <luc.wollants@apptite.be>
2018-08-24 13:11:30 +0200

    VSVGVQ-137 Add ContestParticipation.

---
+++ src/Contest/Models/ContestParticipation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace VSV\GVQ_API\Contest\Models;
+
+use VSV\GVQ_API\Company\ValueObjects\PositiveNumber;
+use VSV\GVQ_API\Contest\ValueObjects\Address;
+use VSV\GVQ_API\Contest\ValueObjects\ContestParticipant;
+use VSV\GVQ_API\Question\ValueObjects\Year;
+use VSV\GVQ_API\Quiz\ValueObjects\QuizChannel;
+
+class ContestParticipation
+{
+    /**
+     * @var Year
+     */
+    private $year;
+
+    /**
+     * @var QuizChannel
+     */
+    private $channel;
+
+    /**
+     * @var ContestParticipant
+     */
+    private $contestParticipant;
+
+    /**
+     * @var Address
+     */
+    private $address;
+
+    /**
+     * @var PositiveNumber
+     */
+    private $answer1;
+
+    /**
+     * @var PositiveNumber
+     */
+    private $answer2;
+}
