f3e2606b1faa6dad4411ea6166bf93da2f93b777
Steven <obex_nielssen@hotmail.com>
2018-08-27 19:25:48 +0200

    VSVGVQ-153 add projector for unique participant

---
+++ src/Quiz/Projectors/UniqueParticipantProjector.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace VSV\GVQ_API\Quiz\Projectors;
+
+use Broadway\Domain\DomainMessage;
+use Broadway\EventHandling\EventListener;
+use VSV\GVQ_API\Quiz\Events\QuizFinished;
+use VSV\GVQ_API\Quiz\Repositories\QuizRepository;
+use VSV\GVQ_API\Quiz\Repositories\UniqueParticipantRepository;
+use VSV\GVQ_API\Quiz\ValueObjects\StatisticsKey;
+
+class UniqueParticipantProjector implements EventListener
+{
+    /**
+     * @var UniqueParticipantRepository
+     */
+    private $uniqueParticipantRepository;
+
+    /**
+     * @var QuizRepository
+     */
+    private $quizRepository;
+
+    /**
+     * @param UniqueParticipantRepository $uniqueParticipantRepository
+     * @param QuizRepository $quizRepository
+     */
+    public function __construct(
+        UniqueParticipantRepository $uniqueParticipantRepository,
+        QuizRepository $quizRepository
+    ) {
+        $this->uniqueParticipantRepository = $uniqueParticipantRepository;
+        $this->quizRepository = $quizRepository;
+    }
+
+    /**
+     * @param DomainMessage $domainMessage
+     */
+    public function handle(DomainMessage $domainMessage)
+    {
+        $payload = $domainMessage->getPayload();
+
+        if ($payload instanceof QuizFinished) {
+            $quiz = $this->quizRepository->getById($payload->getId());
+            $this->uniqueParticipantRepository->add(
+                StatisticsKey::createFromQuiz($quiz),
+                $quiz->getParticipant()->getEmail()
+            );
+        }
+    }
+}
---
+++ tests/Quiz/Projectors/UniqueParticipantProjectorTest.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+
+namespace VSV\GVQ_API\Quiz\Projectors;
+
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use VSV\GVQ_API\Factory\ModelsFactory;
+use VSV\GVQ_API\Quiz\Events\QuizFinished;
+use VSV\GVQ_API\Quiz\Repositories\QuizRepository;
+use VSV\GVQ_API\Quiz\Repositories\UniqueParticipantRepository;
+use VSV\GVQ_API\Quiz\ValueObjects\StatisticsKey;
+
+class UniqueParticipantProjectorTest extends TestCase
+{
+    /**
+     * @var UniqueParticipantRepository|MockObject
+     */
+    private $uniqueParticipantRepository;
+
+    /**
+     * @var QuizRepository|MockObject
+     */
+    private $quizRepository;
+
+    /**
+     * @var UniqueParticipantProjector
+     */
+    private $uniqueParticipantProjector;
+
+    protected function setUp(): void
+    {
+        /** @var UniqueParticipantRepository|MockObject $uniqueParticipantRepository */
+        $uniqueParticipantRepository = $this->createMock(UniqueParticipantRepository::class);
+        $this->uniqueParticipantRepository = $uniqueParticipantRepository;
+
+        /** @var QuizRepository|MockObject $quizRepository */
+        $quizRepository = $this->createMock(QuizRepository::class);
+        $this->quizRepository = $quizRepository;
+
+        $this->uniqueParticipantProjector = new UniqueParticipantProjector(
+            $this->uniqueParticipantRepository,
+            $this->quizRepository
+        );
+    }
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function it_handles_quiz_finished(): void
+    {
+        $quiz = ModelsFactory::createIndividualQuiz();
+
+        $domainMessage = DomainMessage::recordNow(
+            $quiz->getId(),
+            0,
+            new Metadata(),
+            new QuizFinished(
+                $quiz->getId(),
+                11
+            )
+        );
+
+        $this->quizRepository->expects($this->once())
+            ->method('getById')
+            ->with($quiz->getId())
+            ->willReturn($quiz);
+
+        $this->uniqueParticipantRepository->expects($this->once())
+            ->method('add')
+            ->with(
+                StatisticsKey::createFromQuiz($quiz),
+                $quiz->getParticipant()->getEmail()
+            );
+
+        $this->uniqueParticipantProjector->handle($domainMessage);
+    }
+}
