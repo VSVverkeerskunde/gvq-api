50034da12b1292b390a3cba6670644df919f3ca9
Luc Wollants <luc.wollants@apptite.be>
2018-08-31 13:43:30 +0200

    VSVGVQ-137 Add ContestService.

---
+++ src/Contest/Service/ContestService.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace VSV\GVQ_API\Contest\Service;
+
+use Ramsey\Uuid\UuidInterface;
+use VSV\GVQ_API\Contest\Models\ContestParticipation;
+use VSV\GVQ_API\Contest\Repositories\ContestParticipationRepository;
+use VSV\GVQ_API\Question\ValueObjects\Year;
+use VSV\GVQ_API\Quiz\Repositories\QuestionResultRepository;
+use VSV\GVQ_API\Quiz\Repositories\QuizRepository;
+
+class ContestService
+{
+    /**
+     * @var QuestionResultRepository
+     */
+    private $quizResultRepository;
+
+    /**
+     * @var QuizRepository
+     */
+    private $quizRepository;
+
+    /**
+     * @var ContestParticipationRepository
+     */
+    private $contestParticipationRepository;
+
+    /**
+     * Can only participate if 11 or more and no previous participation for given channel.
+     *
+     * @param Year $year
+     * @param UuidInterface $quizId
+     * @return bool
+     */
+    public function canParticipate(Year $year, UuidInterface $quizId): bool
+    {
+        $quizResult = $this->quizResultRepository->getById($quizId);
+        if ($quizResult->getScore() < 11) {
+            return false;
+        }
+
+        $quiz = $this->quizRepository->getById($quizId);
+        $contestParticipation = $this->contestParticipationRepository->getByYearAndEmailAndChannel(
+            $year,
+            $quiz->getParticipant()->getEmail(),
+            $quiz->getChannel()
+        );
+
+        return $contestParticipation === null;
+    }
+
+    /**
+     * @param ContestParticipation $contestParticipation
+     */
+    public function save(ContestParticipation $contestParticipation): void
+    {
+        $existingParticipation = $this->contestParticipationRepository->getByYearAndEmailAndChannel(
+            $contestParticipation->getYear(),
+            $contestParticipation->getContestParticipant()->getEmail(),
+            $contestParticipation->getChannel()
+        );
+
+        if ($existingParticipation === null) {
+            $this->contestParticipationRepository->save($contestParticipation);
+        }
+    }
+}
